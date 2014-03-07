14ccefa097d9b7c925f08c9c6c8df9c18bbc307d
Toby Retallick <toby.retallick@gmail.com>
2014-03-07 17:47:42 +0000

    Removed ability for plane to check weather

--- lib/plane.rb
+++ lib/plane.rb
@@ -14,23 +14,8 @@ class Plane
     @is_flying = true
   end
 
-  def take_off
-    self.fly if self.weather_condition? == :sunny
-  end
-
-  def land
-    self.is_flying = false if self.weather_condition? == :sunny
-  end
-
   def status
     self.is_flying ? :flying : :landed
-  end
-
-  def weather_condition? 
-    case rand(100) + 1
-      when  1..90 then :sunny
-      when 90..100 then :stormy
-    end
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -15,38 +15,6 @@ describe Plane do
     expect(plane.fly).to be_true
   end
 
-  # Test plane with single weather condition
-
-  xit "can take off in sunny weather" do
-    plane.take_off(:sunny)
-    expect(plane.is_flying).to be_true
-  end
-
-  xit "can't take off in stormy weather" do
-    plane.take_off(:stormy)
-    expect(plane.is_flying).to be_false
-  end
-
-  xit "can land in sunny weather" do
-    plane.fly
-    plane.land(:sunny)
-    expect(plane.is_flying).to be_false
-  end
-
-  xit "can't land in stormy weather" do
-    plane.fly
-    plane.land(:stormy)
-    expect(plane.is_flying).to be_true
-  end
-
-  # Test plane with random weather condition (Weather module generates random weather condition)
-
-  it "can take off in sunny weather" do
-    weather = double(:weather, {:condition? => :sunny})
-    plane.fly
-    expect(plane.is_flying).to be_true
-  end
-
   it "can provide its current status (flying or landed)" do
     plane.status
     expect(plane.status).to eq(:landed)
